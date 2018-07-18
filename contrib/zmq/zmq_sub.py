498f1ad55402eac7ea11f6b5e374631e0d537e23
Jeff Hodges - Desktop <jeffhodges93@gmail.com>
2018-07-17 21:47:49 -0700

    Added 60 second sma

--- contrib/zmq/zmq_sub.py
+++ contrib/zmq/zmq_sub.py
@@ -6,6 +6,7 @@ import csv
 import time
 import initialstate
 from multiprocessing import Process, Queue, Lock
+from collections import deque
 
 port = 28332
 
@@ -18,34 +19,61 @@ def write_csv(tstamp, type, value, sequence):
     return True
 
 
-def listener():
-    lock = Lock()
+def thread_manager():
+    lock_ten = Lock()
+    lock_sixty = Lock()
 
     print("Creating Queue")
-    q = Queue()
+    short_queue = Queue()
+    long_queue = Queue()
 
     print("Starting ZMQ Consumer...")
-    worker_1 = Process(target=zmq_tx_consumer, args=(q, lock))
+    worker_1 = Process(target=zmq_tx_consumer, args=(short_queue, lock_ten))
     worker_1.start()
 
     print("Starting Queue Consumer/ InitialState Submitter")
-    worker_2 = Process(target=submit_is, args=(q, lock))
+    worker_2 = Process(target=ten_seconds, args=(short_queue, long_queue, lock_ten, lock_sixty))
     worker_2.start()
 
+    print("Starting 60 Second SMA Consumer/ InititalState Submitter")
+    worker_3 = Process(target=sixty_seconds, args=(long_queue, lock_sixty))
+    worker_3.start()
 
-def submit_is(msg_queue, lock):
+
+def sixty_seconds(sma_queue, lock):
 
     while True:
         lock.acquire()
         try:
-            count = msg_queue.qsize()
+            count = sma_queue.qsize()
+
+            if initialstate.send_log({"10_seconds_sma": (count/6}):
+                print("Submitted the tx count for the last 10 seconds")
+
+            # Clear Queue
+            while not sma_queue.empty():
+                sma_queue.get()
+            print("Cleared Queue")
+
+        finally:
+            lock.release()
+
+        time.sleep(60)
+
+
+def ten_seconds(short_queue, lock_ten):
+
+    while True:
+        lock_ten.acquire()
+        try:
+            count = short_queue.qsize()
 
             if initialstate.send_log({"last_10_secs": count}):
                 print("Submitted the tx count for the last 10 seconds")
 
             # Clear Queue
-            while not msg_queue.empty():
-                msg_queue.get()
+            while not short_queue.empty():
+                short_queue.get()
             print("Cleared Queue")
 
         finally:
@@ -54,13 +82,15 @@ def submit_is(msg_queue, lock):
         time.sleep(10)
 
 
-def zmq_tx_consumer(msg_queue, lock):
+def zmq_tx_consumer(short_queue, long_queue, lock_ten, lock_sixty):
     zmqContext = zmq.Context()
     zmqSubSocket = zmqContext.socket(zmq.SUB)
+
     # zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"hashblock")
     zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"hashtx")
     # zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"rawgovernanceobject")
     # zmqSubSocket.setsockopt(zmq.SUBSCRIBE, b"rawgovernancevote")
+
     zmqSubSocket.connect("tcp://127.0.0.1:%i" % port)
 
     try:
@@ -73,18 +103,35 @@ def zmq_tx_consumer(msg_queue, lock):
             if len(msg[-1]) == 4:
                 msgSequence = struct.unpack('<I', msg[-1])[-1]
                 sequence = str(msgSequence)
-            if topic == "hashblock":
-                print('- HASH BLOCK ('+sequence+') -')
-                hashblock = binascii.hexlify(body).decode("utf-8")
-                write_csv(time.time(), 'hashblock', hashblock, sequence)
-                print(hashblock)
-                # initialstate.send_log({"block_count": "{}".format(sequence)})
+
+            if topic == "hashtx":
+                print('- HASH TX ('+sequence+') -')
+                tx_hash = binascii.hexlify(body).decode("utf-8")
+                print(binascii.hexlify(body).decode("utf-8"))
+
+                lock_ten.acquire()
+
+                try:
+                    short_queue.put(tx_hash)
+                finally:
+                    lock_ten.release()
+
+
+                lock_sixty.acquire()
+
+                try:
+                    long_queue.put(tx_hash)
+                finally:
+                    lock_sixty.release()
+
+
             elif topic == "rawgovernanceobject":
                 print('- RAW GOVERNANCE OBJECT ('+sequence+') -')
                 governance_object = binascii.hexlify(body).decode("utf-8")
                 write_csv(time.time(), 'rawgovernanceobject', governance_object, sequence)
                 print(governance_object)
                 # initialstate.send_log({"hash": governance_object, "tx_count": sequence})
+
             elif topic == "rawgovernancevote":
                 print('- RAW GOVERNANCE VOTE ('+sequence+') -')
                 governance_vote = binascii.hexlify(body).decode("utf-8")
@@ -92,27 +139,25 @@ def zmq_tx_consumer(msg_queue, lock):
                 print(governance_vote)
                 # initialstate.send_log({"hash": governance_vote, "tx_count": sequence})
 
-            elif topic == "hashtx":
-                print('- HASH TX ('+sequence+') -')
-                tx_hash = binascii.hexlify(body).decode("utf-8")
-                # initialstate.send_log({"hash": tx_hash, "tx_count": sequence})
-                print(binascii.hexlify(body).decode("utf-8"))
-
-                lock.acquire()
-                try:
-                    msg_queue.put(tx_hash)
-                finally:
-                    lock.release()
+            elif topic == "hashblock":
+                print('- HASH BLOCK ('+sequence+') -')
+                hashblock = binascii.hexlify(body).decode("utf-8")
+                write_csv(time.time(), 'hashblock', hashblock, sequence)
+                print(hashblock)
+                # initialstate.send_log({"block_count": "{}".format(sequence)})
 
             elif topic == "hashtxlock":
                 print('- HASH TX LOCK ('+sequence+') -')
                 print(binascii.hexlify(body).decode("utf-8"))
+
             elif topic == "rawblock":
                 print('- RAW BLOCK HEADER ('+sequence+') -')
                 print(binascii.hexlify(body[:80]).decode("utf-8"))
+
             elif topic == "rawtx":
                 print('- RAW TX ('+sequence+') -')
                 print(binascii.hexlify(body).decode("utf-8"))
+
             elif topic == "rawtxlock":
                 print('- RAW TX LOCK ('+sequence+') -')
                 print(binascii.hexlify(body).decode("utf-8"))
@@ -124,4 +169,4 @@ def zmq_tx_consumer(msg_queue, lock):
 
 
 if __name__ == '__main__':
-    listener()
+    thread_manager()
